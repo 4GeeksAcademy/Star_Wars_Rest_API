b0e448c49f496893319e87f74b8a5c0bb605015e
Mariana <101410421+Placito@users.noreply.github.com>
2023-08-22 17:13:34 +0000

    adding routes

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,104 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# Users
+@app.route('/users', methods=['GET'])
+@app.route('/users/<int:user_id>', methods=['GET'])
+def handle_users(user_id=None):
+    if user_id is None:
+        users = User.query.all()
+        return jsonify([x.serialize() for x in users]), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    user = User.query.filter_by(id=user_id).first()
+    if user is not None:
+        return jsonify(user.serialize()), 200
 
-    return jsonify(response_body), 200
+    return jsonify({"msg": "Request not valid"}), 400
+
+
+# Favorites
+@app.route('/users/<int:user_id>/favorites', methods=['GET', 'POST', 'DELETE'])
+def handle_favorites(user_id=None):
+    if user_id is None:
+        return jsonify({"msg": "This endpoint needs a user_id"}), 400
+
+    if request.method == 'GET':
+        favorites = Favorite.query.filter_by(id=user_id).all()
+        return jsonify([x.serialize() for x in favorites]), 200
+
+    if request.method == 'POST':
+        body = json.loads(request.data)
+
+        category_id = Category.query.filter_by(
+            category_name=body['category_name']).first()
+
+        if category_id is None:
+            return jsonify({"msg": f"{body['category_name']} is not a valid category"}), 400
+
+        favorite = Favorite(
+            user_id=user_id,
+            category_id=category_id.id,
+            category_fk_id=body['category_fk_id']
+        )
+
+        db.session.add(favorite)
+        db.session.commit()
+
+        return jsonify({
+            "msg": f"Favorite added",
+            "inserted_id": f"{favorite.id}"
+        }), 200
+
+    if request.method == 'DELETE':
+        body = json.loads(request.data)
+
+        category = Category.query.filter_by(
+            category_name=body['category_name']).first()
+
+        if category is None:
+            return jsonify({"msg": f"{body['category_name']} is not a category"}), 400
+
+        db.session.delete(category)
+        db.session.commit()
+
+        return jsonify({
+            "msg": f"Category deleted",
+            "deleted_id": f"{category.id}"
+        }), 200
+
+    return jsonify({"msg": f"{request.method}: Request not valid"}), 400
+
+
+# Characters
+@app.route('/characters', methods=['GET'])
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def handle_characters(character_id=None):
+    if character_id is None:
+        characters = Character.query.all()
+        characters = [x.serialize() for x in characters]
+        return jsonify(characters), 200
+
+    character = Character.query.filter_by(id=character_id).first()
+    if character is not None:
+        return jsonify(character.serialize()), 200
+
+    return jsonify({"msg": "Request not valid"}), 400
+
+
+# Planets
+@app.route('/planets', methods=['GET'])
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def handle_planets(planet_id=None):
+    if planet_id is None:
+        planets = Planet.query.all()
+        planets = [x.serialize() for x in planets]
+        return jsonify(planets), 200
+
+    planet = Planet.query.filter_by(id=planet_id).first()
+    if planet is not None:
+        return jsonify(planet.serialize()), 200
+
+    return jsonify({"msg": "Request not valid"}), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -16,4 +19,76 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    character_id= db.Column(db.Integer, db.ForeignKey("character.id"), nullable=False)
+    planet_id= db.Column(db.Integer, db.ForeignKey("planet.id"), nullable=False)
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": self.character_id,
+            "planet_id": self.planet_id,
+        }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(250), nullable=True)
+    gender = db.Column(db.String(120), nullable=True)
+    hair_color = db.Column(db.String(120), nullable=True)
+    eye_color = db.Column(db.String(120), nullable=True)
+    birth_year = db.Column(db.String(120), nullable=True)
+    rotation_period = db.Column(db.String(120), nullable=True)
+    height = db.Column(db.String(3), nullable=True)
+    skin_color = db.Column(db.String(120), nullable=True)
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "rotation_period": self.rotation_period,
+            "height": self.height,
+            "skin_color": self.skin_color,
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False, unique=True)
+    description = db.Column(db.String(250), nullable=True)
+    climate = db.Column(db.String(120), nullable=True)
+    population = db.Column(db.String(120), nullable=True)
+    orbital_period = db.Column(db.String(120), nullable=True)
+    rotation_period = db.Column(db.String(120), nullable=True)
+    diameter = db.Column(db.String(3), nullable=True)
+    terrain = db.Column(db.String(120), nullable=True)
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "climate": self.climate,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+            "terrain": self.terrain,
         }
